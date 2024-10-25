8cf8a539729421325bc98d8059d97885f55435cf
SimonDierickx <simon.dierickx@student.hogent.be>
2024-10-25 13:07:47 +0200

    kleur

--- sample-app.sh
+++ sample-app.sh
@@ -10,7 +10,8 @@ cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "body {background: blue;}" > static/styles.css
+rm -rf static/styles.css
+echo "body {background: blue;}" > static/style.css
 echo kaas
 
 cat > tempdir/Dockerfile << _EOF_
